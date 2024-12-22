a53df1cd8de307041a6627fb8c1a280dd091d59e
EnVullin <vullings@hccnet.nl>
2024-12-22 13:55:34 +0100

    First commit, working hook NON DLL

---
+++ KeyboardHookDLL.lpr
@@ -0,0 +1,52 @@
+library KeyboardHookDLL;
+
+{$mode objfpc}{$H+}
+
+
+uses
+  Windows, Messages, SysUtils, Classes;
+
+const
+  WH_KEYBOARD_LL = 13;
+
+var
+  HookHandle: HHOOK = 0;
+  LastMessage : String = '';
+
+function KeyboardProc(nCode: Integer; wParam: WPARAM; lParam: LPARAM): LRESULT; stdcall;
+begin
+  if nCode = HC_ACTION then
+  begin
+    LastMessage := IntToStr( nCode ) + ' ' + IntToStr( wParam ) + ' ' + IntToStr( lParam );
+    // Hier kun je je eigen logica toevoegen
+  end;
+  Result := CallNextHookEx(HookHandle, nCode, wParam, lParam);
+end;
+
+procedure InstallHook; stdcall;
+begin
+  HookHandle := SetWindowsHookEx( WH_KEYBOARD_LL, @KeyboardProc, HInstance, 0);
+end;
+
+procedure UninstallHook; stdcall;
+begin
+  if HookHandle <> 0 then
+  begin
+    UnhookWindowsHookEx(HookHandle);
+    HookHandle := 0;
+  end;
+end;
+
+Procedure GetLastMsg( Var LastMsg : String ); stdcall;
+Begin
+  LastMsg := LastMessage;
+end;
+
+exports
+  InstallHook,
+  UninstallHook,
+  GetLastMsg;
+
+begin
+end.
+
---
+++ backup/main.pas
@@ -0,0 +1,125 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Messages, Windows; // Two extra units
+
+const
+  WH_KEYBOARD_LL = 13; // Keyboard hook constant
+  WM_KEYDOWN = $0100;  // KeyDown message
+
+type
+  TInstallHook = procedure; stdcall;
+  TUninstallHook = procedure; stdcall;
+  TGetLastMsg = Procedure( var LastMsg : string ); StdCall;
+
+  { TForm1 }
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    ListBox1: TListBox;
+    PlaceHookBtn: TButton;
+
+    Button1: TButton;
+    UnHookBtn: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure PlaceHookBtnClick(Sender: TObject);
+    procedure UnHookBtnClick(Sender: TObject);
+  private
+    HookHandle : HHOOK; // Hook address
+
+    Procedure LoadDLL;
+    Procedure UnLoadDLL;
+
+  public
+
+  end;
+
+
+
+var
+  Form1        : TForm1;
+  DLLHandle    : THandle;
+  InstallHook  : TInstallHook;
+  UninstallHook: TUninstallHook;
+  LastMsg      : TGetLastMsg;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+
+procedure TForm1.LoadDLL;
+begin
+  DLLHandle := LoadLibrary('KeyboardHookDLL.dll');
+  if DLLHandle <> 0 then
+  begin
+    InstallHook   := TInstallHook(   GetProcAddress ( DLLHandle, 'InstallHook'  ));
+    UninstallHook := TUninstallHook( GetProcAddress ( DLLHandle, 'UninstallHook'));
+    LastMsg       := TGetLastMsg(       GetProcAddress ( DLLHandle, 'GetLastMsg'      ));
+    if Assigned(InstallHook) and Assigned(UninstallHook) and Assigned(LastMsg) then
+    begin
+      InstallHook;
+    end
+    else
+    begin
+      ShowMessage('Kan functies niet vinden in DLL.');
+    end;
+  end
+  else
+  begin
+    ShowMessage('Kan DLL niet laden.');
+  end;
+end;
+
+procedure TForm1.UnloadDLL;
+begin
+  if DLLHandle <> 0 then
+  begin
+    UninstallHook;
+    FreeLibrary(DLLHandle);
+    DLLHandle := 0;
+  end;
+end;
+
+
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Self.close;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+Var Line : string;
+begin
+  LastMsg( Line );
+  Self.ListBox1.Items.Inster(0, Line );
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Self.ListBox1.Clear;
+end;
+
+
+procedure TForm1.PlaceHookBtnClick(Sender: TObject);
+begin
+    Self.LoadDLL;
+end;
+
+procedure TForm1.UnHookBtnClick(Sender: TObject);
+begin
+  Self.UnloadDLL;
+end;
+
+end.
+
